d9691b572bf8b3035511f8a75de10b5def200180
rychersky <rychersky@gmail.com>
2024-06-18 12:43:02 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,38 @@
 import { useEffect, useState } from 'react';
 
+const numbers = [1, 4, 7, 10, 13, 16, 19, 21, 23, 25];
+
 export default function App() {
-  const [data, setData] = useState();
+  const [pokemon, setPokemon] = useState();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    async function fetchData() {
-      const result = await fetch('https://pokeapi.co/api/v2/pokemon/1/');
-      setData(result);
-    }
+    const result = [];
 
-    fetchData();
+    numbers.forEach((num) => {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${num}/`)
+        .then((res) => res.json())
+        .then((data) => result.push(data));
+    });
+
+    setPokemon(result);
+    setLoading(false);
   }, []);
 
   return (
     <>
-      {data ? <p onClick={() => console.log(data)}>test</p> : <p>loading</p>}
-      <p>stuff</p>
+      {loading ? <p>loading</p> : <p>done</p>}
+      <p
+        onClick={() => {
+          console.log('⬜️⬜️⬜️⬜️⬜️');
+          console.log('numbers:', numbers);
+          console.log('pokemon:', pokemon);
+          console.log(pokemon?.length >= numbers.length);
+        }}
+      >
+        pokemon length: {pokemon?.length}
+      </p>
+      <p>numbers length: {numbers.length}</p>
     </>
   );
 }
-
-const numbers = [1, 4, 7, 10, 13, 16, 19, 21, 23, 25];
