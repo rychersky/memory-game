6c5c3bc942218736643a2ddde54d3ae4fd41483c
rychersky <rychersky@gmail.com>
2024-06-19 18:23:33 -0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
 import { useEffect, useState } from 'react';
+import './app.scss';
+
+function shuffle(array) {
+  const result = [...array];
+  for (let i = result.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [result[i], result[j]] = [result[j], result[i]];
+  }
+  return result;
+}
 
 export default function App() {
-  const [pokemon, setPokemon] = useState();
+  const [pokemon, setPokemon] = useState([]);
+  const [score, setScore] = useState(0);
+  const [playerLoses, setPlayerLoses] = useState(false);
+  const [playerWins, setPlayerWins] = useState(false);
+  const [namesPicked, setNamesPicked] = useState([]);
 
   useEffect(() => {
     async function getPokemon() {
       const data = await Promise.all(
         [1, 4, 7, 10, 13, 16, 19, 21, 23, 25].map(async (n) => {
-          const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${n}/`);
-          return data.json();
+          const response = await fetch(
+            `https://pokeapi.co/api/v2/pokemon/${n}/`
+          );
+          const data = await response.json();
+          const name = `${data.name.charAt(0).toUpperCase()}${data.name.slice(
+            1
+          )}`;
+          const imgUrl = data.sprites.front_default;
+          return { name, imgUrl };
         })
       );
 
@@ -18,5 +39,62 @@ export default function App() {
     getPokemon();
   }, []);
 
-  return <>{pokemon ? <p>finished</p> : <p>loading</p>}</>;
+  useEffect(() => {
+    if (score === 10) {
+      setPlayerWins(true);
+    }
+  }, [score]);
+
+  return (
+    <div className="app">
+      <header>
+        <h1>Memory Game</h1>
+        <p className="score">Score: {score}</p>
+        <p className="instructions">10 points wins the game</p>
+      </header>
+      <main>
+        {pokemon && !playerLoses && !playerWins && (
+          <div className="game">
+            {pokemon.map((p) => (
+              <button
+                type="button"
+                className="card"
+                key={p.name}
+                onClick={() => {
+                  if (namesPicked.includes(p.name)) {
+                    setPlayerLoses(true);
+                  } else {
+                    setNamesPicked([...namesPicked, p.name]);
+                    setScore(score + 1);
+                    setPokemon(shuffle(pokemon));
+                  }
+                }}
+              >
+                <h2>{p.name}</h2>
+                <img src={p.imgUrl} alt={p.name} />
+              </button>
+            ))}
+          </div>
+        )}
+
+        {(playerLoses || playerWins) && (
+          <div className="lost-game">
+            {playerLoses && <p>Oh no you lost 🥺</p>}
+            {playerWins && <p>🥳 You won! 🥳</p>}
+            <button
+              type="button"
+              onClick={() => {
+                setPlayerLoses(false);
+                setPlayerWins(false);
+                setScore(0);
+                setNamesPicked([]);
+              }}
+            >
+              Play again
+            </button>
+          </div>
+        )}
+      </main>
+    </div>
+  );
 }
